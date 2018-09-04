724f15a50357a6958b773c74a4045a8c5b08005b
Paul Roberts <paul.roberts@valtech.co.uk>
2018-09-04 10:23:53 +0100

    Suppress warnings on empty list where desired nulls

--- src/main/java/uk/gov/dft/bluebadge/service/applicationmanagement/converter/collection/ApplicationToEntityCollection.java
+++ src/main/java/uk/gov/dft/bluebadge/service/applicationmanagement/converter/collection/ApplicationToEntityCollection.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@SuppressWarnings("squid:S1168")
 interface ApplicationToEntityCollection<E, M> {
   E mapToEntity(M model, UUID applicationId);
 
