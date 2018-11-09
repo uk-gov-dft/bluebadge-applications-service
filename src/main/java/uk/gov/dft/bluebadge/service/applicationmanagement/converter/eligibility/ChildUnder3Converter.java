199c723da06011a692a269ef3a0f6406e535b07a
Robert Smallwood <rob.smallwood@valtech.co.uk>
2018-11-09 10:39:51 +0000

    BBB-1015 - Ensured the bulk eqip other desc is only passed when the OTHER type code is used

--- src/main/java/uk/gov/dft/bluebadge/service/applicationmanagement/converter/eligibility/ChildUnder3Converter.java
+++ src/main/java/uk/gov/dft/bluebadge/service/applicationmanagement/converter/eligibility/ChildUnder3Converter.java
@@ -24,7 +24,9 @@ public class ChildUnder3Converter implements ApplicationBiConverter {
       ChildUnder3 childUnder3 = model.getEligibility().getChildUnder3();
       childUnder3.setBulkyMedicalEquipmentTypeCode(
           BulkyMedicalEquipmentTypeCodeField.fromValue(entity.getBulkyEquipmentTypeCode()));
-      childUnder3.setOtherMedicalEquipment(entity.getBulkyEquipmentOtherDesc());
+      if (BulkyMedicalEquipmentTypeCodeField.OTHER == childUnder3.getBulkyMedicalEquipmentTypeCode()) {
+        childUnder3.setOtherMedicalEquipment(entity.getBulkyEquipmentOtherDesc());
+      }
     }
   }
 
@@ -36,7 +38,9 @@ public class ChildUnder3Converter implements ApplicationBiConverter {
 
     entity.setBulkyEquipmentTypeCode(
         model.getEligibility().getChildUnder3().getBulkyMedicalEquipmentTypeCode().name());
-    entity.setBulkyEquipmentOtherDesc(
+    if (BulkyMedicalEquipmentTypeCodeField.OTHER == BulkyMedicalEquipmentTypeCodeField.fromValue(entity.getBulkyEquipmentTypeCode())) {
+      entity.setBulkyEquipmentOtherDesc(
         model.getEligibility().getChildUnder3().getOtherMedicalEquipment());
+    }
   }
 }
