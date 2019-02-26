b952ab3241bf09d79a7314055a9115ca023dda9c
Andy Rea <andrew.rea@andrewreaassociates.com>
2019-02-26 15:03:47 +0000

    Changing to BRANCH_NAME for now to get this workingf

--- acceptance-tests/run-regression.sh
+++ acceptance-tests/run-regression.sh
@@ -14,9 +14,9 @@ tearDown() {
     echo "Pruning docker containers/images"
     docker system prune -a -f
 
-    if [[ -d "dev-env-develop" ]]; then
-      echo "Tearing down existing dev-env-develop directory"
-      rm -rf "dev-env-develop"
+    if [[ -d "dev-env-$BRANCH_NAME" ]]; then
+      echo "Tearing down existing dev-env-$BRANCH_NAME directory"
+      rm -rf "dev-env-$BRANCH_NAME"
     fi
 }
 
@@ -58,10 +58,10 @@ fi
 tearDown
 
 # Get the dev-env stuff
-echo "**************************** Retrieving dev-env (develop) scripts."
-curl -sL -H "Authorization: token $(cat ~/.ssh/github_token)" "https://github.com/uk-gov-dft/dev-env/archive/develop.tar.gz" | tar xz
+echo "**************************** Retrieving dev-env ($BRANCH_NAME) scripts."
+curl -sL -H "Authorization: token $(cat ~/.ssh/github_token)" "https://github.com/uk-gov-dft/dev-env/archive/$BRANCH_NAME.tar.gz" | tar xz
 if [ $? -ne 0 ]; then
-   echo "Cannot download dev-env!"
+   echo "Cannot download dev-env ($BRANCH_NAME)!"
    exit 1
 fi
 
@@ -69,13 +69,13 @@ fi
 gradle :outputComputedVersion
 
 . "dev-env-develop/env.sh"
-if ! [[ "develop" =~ ^develop.*|^release.* ]]; then
+if ! [[ "$BRANCH_NAME" =~ ^develop.*|^release.* ]]; then
    . env-feature.sh
 fi
 
 outputVersions
 
-cd "dev-env-develop"
+cd "dev-env-$BRANCH_NAME"
 
 docker-compose build
 docker-compose up -d --no-color
